3336b3955b14b04a19dc743e6fa47bd6bd0e18dc
tanbir ahammad <tanvirahammadjoy@gmail.com>
2026-04-14 00:28:41 +0800

    add readline interface for user input and create package files

--- 01_learning_basic/script.js
+++ 01_learning_basic/script.js
@@ -1,1 +1,11 @@
-console.log("hello world!");
+import readline from "readline";
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("what is your name: ", (name) => {
+  console.log(`your name is ${name}`);
+  rl.close();
+});
